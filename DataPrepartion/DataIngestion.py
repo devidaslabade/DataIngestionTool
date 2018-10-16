a69195f511d476d97a40ebb6706b705ce1228d92
Sumit Kumar <ursumit@gmail.com>
2018-10-16 14:23:27 +0530

    Adding colmapping

--- DataPrepartion/DataIngestion.py
+++ DataPrepartion/DataIngestion.py
@@ -29,8 +29,8 @@ def launchSpark(src,schema,trgt):
         df.show()
         df.printSchema()
         print(trgt)
-        print(trgt["TrgLocation"]+"/"+trgt["TrgType"])
-        df.write.mode('overwrite').format(trgt["TrgType"]).save(trgt["TrgLocation"]+"/"+trgt["TrgType"])
+        print(trgt["DestLocation"]+"/"+trgt["DestType"])
+        df.write.mode('overwrite').format(trgt["DestType"]).save(trgt["DestLocation"]+"/"+trgt["DestType"])
     
     #orc_df.write.orc(
 
@@ -50,7 +50,7 @@ def launchSpark(src,schema,trgt):
 
 if __name__ == "__main__" :
     src = json.loads(open('..\source\src.json', encoding='utf-8').read())
-    srcColMap = json.loads(open('..\source\srcColMap.json', encoding='utf-8').read())
+    srcColMap = json.loads(open('..\source\srcCols.json', encoding='utf-8').read())
     dest = json.loads(open('..\dest\dest.json', encoding='utf-8').read())
     prc = json.loads(open('..\process\prc.json', encoding='utf-8').read())
     #pprint(json)
@@ -59,7 +59,7 @@ if __name__ == "__main__" :
         #fields = [StructField(x['colName'], StringType(), False) for x in srcColMap[key]]
         fields=fetchSchema(srcColMap[prcVal['SrcId']])             
         schema = StructType(fields)
-        launchSpark(src[prcVal['SrcId']],schema,dest[prcVal['TrgId']])        
+        launchSpark(src[prcVal['SrcId']],schema,dest[prcVal['DestId']])        
         #launchSpark(src[value['SrcId']]['SrcLocation'],schema,dest[value['TrgId']])
 
     
