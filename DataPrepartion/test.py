a515ce4bb9d4d6ae9fb9465623b4b0dd665f86a5
ankesh1702 <ankesh.jain2@teradata.com>
2018-10-18 11:15:23 +0530

    commiting before merge

--- DataPrepartion/test.py
+++ DataPrepartion/test.py
@@ -1,5 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 16 17:46:25 2018
+
+@author: aj250046
+"""
+
+from pyspark.sql import SparkSession
 import json
+from pprint import pprint
+from pyspark.sql import HiveContext
+from pyspark.sql.types import *
+from pyspark import SparkContext
 
-#trg = json.loads(open('..\target\trg.json', encoding='utf-8').read())
+# from pyspark.sql.types import StructType, StructField
+# from pyspark.sql.types import DoubleType, IntegerType, StringType
 
-tes=json.loads(open('..\dest\dest.json' ,encoding='utf-8').read())
+
+def fetchSchema(srcCols):
+    print("Fetching schema values of ", srcCols)
+    fields = []
+    for clm in srcCols:
+        print(clm['isNullable'])
+        if clm['colType'] == "String":
+            colField = StructField(clm['colName'], StringType(), eval(clm['isNullable']))
+            fields.append(colField)
+        elif clm['colType'] == "Integer":
+            colField = StructField(clm['colName'], IntegerType(), eval(clm['isNullable']))
+            fields.append(colField)
+        else:
+            colField = StructField(clm['colName'], StringType(), eval(clm['isNullable']))
+            fields.append(colField)
+    return fields
+
+
+def launchSpark(src, schema, trgt):
+    spark = SparkSession.builder.appName("DataIngestion").enableHiveSupport.getOrCreate()
+    df = None
+    if src['SrcType'] == "csv":
+        df = spark.read.schema(schema).option("header", src['Header']).csv(src['SrcLocation'])
+    elif src['SrcType'] == "json":
+        df = spark.read.schema(schema).option("header", src['Header']).csv(src['SrcLocation'])
+    elif src['SrcType'] == "parquet":
+        df = spark.read.schema(schema).option("header", src['Header']).csv(src['SrcLocation'])
+    elif src['SrcType'] == "orc":
+        df = spark.read.schema(schema).option("header", src['Header']).csv(src['SrcLocation'])
+    elif src['SrcType'] == "hivetable":
+        df = spark.read.schema(schema).option("header", src['Header']).csv(src['SrcLocation'])
+        df.write.format("orc").saveAsTable("employees")
+
+
+    df.show()
+    df.printSchema()
+    print(trgt)
+    print(trgt["DestLocation"] + "/" + trgt["DestType"])
+    df.write.mode('overwrite').format(trgt["DestType"]).save(trgt["DestLocation"] + "/" + trgt["DestType"])
+
+    # orc_df.write.orc(
+
+
+if __name__ == "__main__":
+    src = json.loads(open('..\source\src.json', encoding='utf-8').read())
+    srcColMap = json.loads(open('..\source\srcCols.json', encoding='utf-8').read())
+    dest = json.loads(open('..\dest\dest.json', encoding='utf-8').read())
+    prc = json.loads(open('..\process\prc.json', encoding='utf-8').read())
+    # pprint(json)
+    for prcKey, prcVal in prc.items():
+        print(prcVal['PrcName'])
+        # fields = [StructField(x['colName'], StringType(), False) for x in srcColMap[key]]
+        fields = fetchSchema(srcColMap[prcVal['SrcId']])
+        schema = StructType(fields)
+        launchSpark(src[prcVal['SrcId']], schema, dest[prcVal['DestId']])
+        # launchSpark(src[value['SrcId']]['SrcLocation'],schema,dest[value['TrgId']])
+
