cffd0095fa661b9e151db2a1000b1b876f8e2a10
Harshit Goyal <goyyal.harsh@gmail.com>
2019-01-18 15:26:29 +0530

    commiting setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@ import zipfile
 from setuptools import setup, find_packages
 
 from distutils.cmd import Command
-from pip.commands import WheelCommand
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.commands import WheelCommand
+except ImportError: # for pip <= 9.0.3
+    from pip.commands import WheelCommand
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
 PACKAGE_NAME = 'dataIngestionTool'
